8033459b83bf1a8108bf1e47312d1a105afa3c45
lancevuu <lancevu32@gmail.com>
2025-03-16 04:18:50 +0000

    last fix

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -1,38 +1,25 @@
 #include "dijkstras.h"
+#include <algorithm>
 #include <queue>
 #include <iostream>
-#include <algorithm>
-
-using namespace std;
-
-struct Node {
-    int vertex;
-    int weight;
-    Node(int v, int w) : vertex(v), weight(w) {}
-
-    bool operator>(const Node& other) const {
-        return weight > other.weight;
-    }
-};
 
 vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous) {
     int n = G.numVertices;
-    vector<int> distance(n, INF);
+    vector<int> distance(n, INF); // initializes vectors
     vector<bool> visited(n, false);
-    priority_queue<Node, vector<Node>, greater<Node>> pq;
+    priority_queue<Node, vector<Node>, greater<Node>> pq; // pq initialized
     pq.push(Node(source, 0));
+    previous.resize(n, -1);
     distance[source] = 0;
-    previous.assign(n, -1);
-
-    while (!pq.empty()) {
-        Node current = pq.top();
+    while (!pq.empty()) { // processes each node until pq is empty
+        Node currentNode = pq.top();
         pq.pop();
-        int u = current.vertex;
+        int u = currentNode.vertex;
         if (visited[u]) continue;
         visited[u] = true;
-        for (const Edge& neighbor : G[u]) {
-            int v = neighbor.dst;
-            int weight = neighbor.weight;
+        for (const Edge& edge : G[u]) {
+            int v = edge.dst;
+            int weight = edge.weight;
             if (!visited[v] && distance[u] + weight < distance[v]) {
                 distance[v] = distance[u] + weight;
                 previous[v] = u;
@@ -40,18 +27,19 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
             }
         }
     }
-
-    return distance;
+    return distance; //  Dijkstra's algorithm, computing the shortest paths from the source node to all other nodes
 }
+
 
 vector<int> extract_shortest_path(const vector<int>& distances, const vector<int>& previous, int destination) {
     vector<int> path;
     if (distances[destination] == INF)
         return path;
-    for (int v = destination; v != -1; v = previous[v])
-        path.push_back(v);
+    for (int node = destination; node != -1; node = previous[node]) {
+        path.push_back(node);
+    }
     reverse(path.begin(), path.end());
-    return path;
+    return path; // finds the shortest path
 }
 
 void print_path(const vector<int>& path, int total) {
@@ -59,7 +47,9 @@ void print_path(const vector<int>& path, int total) {
         cout << "\nTotal cost is " << total << "\n";
         return;
     }
-    for (size_t i = 0; i < path.size(); ++i)
-        cout << path[i] << " ";
-    cout << "\nTotal cost is " << total << "\n";
-}
+    cout << path[0];
+    for (size_t i = 1; i < path.size(); ++i) {
+        cout << " " << path[i];
+    }
+    cout << " \nTotal cost is " << total << "\n";
+} // prints out the path from Dijkstra's
